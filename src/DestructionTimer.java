6a9b066d97eb94ee936e69d9480c798a7bc25638
Szikszai Levente <szikilevi01@gmail.com>
2023-04-14 13:55:01 +0200

    Game és DestructionTimer létrehozása

---
+++ src/DestructionTimer.java
@@ -0,0 +1,15 @@
+
+public class DestructionTimer {
+
+	Pump[] pumps;
+	
+	void tick()
+	{
+		System.out.println("tick");
+	}
+	
+	void addPump(Pump p)
+	{
+		System.out.println("addPump");
+	}
+}
--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,89 @@
 
 public class Game {
 
+	//Repairman[] repairmanGroup;
+	//Saboteur[] saboteurGroup;
+	int repairmanPoints;
+	int saboteurPoints;
+	Element[] gameElements;
+	Cistern[] cisterns;
+	//saboteurPointSource[] saboteurPointSources;
+	int remainingRounds;
+	DestructionTimer timer;
+	
+	public Game()
+	{
+		
+	}
+	
+	public void initialize()
+	{
+		System.out.println("inicialize");
+	}
+	
+	public void playGame()
+	{
+		System.out.println("playGame");
+	}
+	
+	public void endGame()
+	{
+		System.out.println("endGame");
+	}
+	
+	public void endTurn()
+	{
+		System.out.println("endTurn");
+	}
+	
+	public void SimulateWaterflow()
+	{
+		System.out.println("SimulateWaterFlow");
+	}
+	
+	public void addElement(Element e)
+	{
+		System.out.println("addEmelent");
+	}
+	
+	public Element[] getGameElements()
+	{
+		
+		System.out.println("getGameElements");
+		return gameElements;
+		
+	}
+	
+	public void addSaboteur()
+	{
+		//saboteurGroup.add
+		System.out.println("addSaboteur");
+	}
+	
+	public void addRepairman()
+	{
+		//repairmanGroup.add
+		System.out.println("addRepairman");
+	}
+	
+	public void addPipe(Pipe p)
+	{
+		System.out.println("addPipe");
+		//saboteurSourcePoints.add
+	}
+	
+	public void calculatePoints()
+	{
+		System.out.println("calculatePoints");
+	}
+	
+	public void addPump(Pump p)
+	{
+		System.out.println("addPump");
+	}
+	
+	public void newPipe()
+	{
+		System.out.println("newPipe");
+	}
 }
