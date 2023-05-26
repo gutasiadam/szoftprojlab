65a2f19096dcbedda2bfcb625aa05ab639d98c5f
Szikszai Levente <szikilevi01@gmail.com>
2023-05-26 23:58:16 +0200

    actionPerformed skeleton

--- src/ActionButton.java
+++ src/ActionButton.java
@@ -1,7 +1,23 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
 
 public class ActionButton extends JButton{
-    private ArrayList<Integer> actions;
+    private ArrayList<Integer> actions = new ArrayList<Integer>();
+
+    ActionButton(ArrayList<Integer> actions)
+    {
+        this.actions=actions;
+        addActionListener(
+        new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+            }
+        });
+    }
 }
--- src/ElementButton.java
+++ src/ElementButton.java
@@ -1,11 +1,25 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class ElementButton extends JButton{
-    private ArrayList<ImageIcon> statusimages;
+    private ArrayList<ImageIcon> statusimages = new ArrayList<ImageIcon>();
     private Element element;
 
-    
+    ElementButton(Element element)
+    {
+        this.element=element;
+        addActionListener(
+        new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+            }
+        });
+    }
 }
