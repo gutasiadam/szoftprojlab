12b167674a01e1d2c5ae14a2d457be58a2443cc6
Szikszai Levente <szikilevi01@gmail.com>
2023-05-26 20:45:08 +0200

    Control Comments

--- src/Control.java
+++ src/Control.java
@@ -1,12 +1,20 @@
 public class Control {
 
+    //Az egyetlen Control objektum
     private static Control instance;
 
     private Repairman r;
     private Saboteur s;
 
+    /**
+     * Privát konstruktor, mivel Singleton
+     */
     private Control(){}
 
+    /**
+     * Visszaadja a Singleton Control objektumot
+     * @return Singleton Control objektum
+     */
     public static Control getInstance(){
         if (instance==null){
             instance=new Control();
@@ -14,14 +22,24 @@ public class Control {
         return instance;
     }
 
+    /**
+     * Ezen a függvényen keresztül tudjuk átmozgatni a játékost adott irányba.
+     * @param dir Hanyadik szomszédjára lépjen át az Element-ről amin áll.
+     */
     public void Move(int dir){
-        //Le kéne tudni kérdezni hogy éppen hol áll a karakter hogy onnnan lehessen removolni
+        Game.getInstance().getCurrentCharacter().move(dir);
     }
 
+    /**
+     * Ezen a függvényen keresztül tudjuk kilyukasztani az Element-et amin állunk
+     */
     public void Stab(){
         Game.getInstance().getCurrentCharacter().dealDamage();
     }
 
+    /**
+     * Ezen a függvényen keresztül tudjuk lehelyezni az aktuális játékosnál lévű Pumpát
+     */
     public void PlacePump(){
         //Elvileg ha biztosítjuk, hogy ezt csak a szerelők hívhatják meg akkor nem gyilkolnak meg a castolásért (meg az nyilván a többi fv-re is igaz alatta)
         //Ha viszont nem így akarjátok akkor minden fv kell a character osztályba, hogy meg lehessen azokat hívni
@@ -29,40 +47,67 @@ public class Control {
         r.PlacePump();
     }
 
+    /**
+     * Ezen a függvényen keresztül tudjuk lehelyezni az aktuális játékosnál lévő Csövet
+     */
     public void PlacePipe(){
         r=(Repairman)Game.getInstance().getCurrentCharacter();
         r.PlacePipe();
     }
 
+    /**
+     * Ezen a függvényen keresztül tudunk Pumpát felvenni az aktuális játékossal.
+     */
     public void PickupPump(){
         r=(Repairman)Game.getInstance().getCurrentCharacter();
         r.LiftPump();
     }
 
+    /**
+     * Ezen a függvényen keresztül tudunk adott csövet felvenni az aktuális játékossal
+     * @param dir Hanyadik szomszédját akarjuk felemelni az Element-nek amin áll.
+     */
     public void PickUpPipe(int dir){
         r=(Repairman)Game.getInstance().getCurrentCharacter();
         r.LiftPipe(dir);
     }
 
+    /**
+     * Ezen a függvényen keresztül tudjuk megjavítani az Element-et amin áll az aktuális játékos
+     */
     public void Repair(){
         r=(Repairman)Game.getInstance().getCurrentCharacter();
         r.RepairElement();
     }
 
+    /**
+     * Ezen a függvényen keresztül tudjuk ragadóssá tenni az Element-et áll az aktuális játékos
+     */
     public void Stick(){
         s=(Saboteur)Game.getInstance().getCurrentCharacter();
         s.makeSticky();
     }
 
+    /** 
+    * Ezen a függvényen keresztül tudjuk csúszóssá tenni az Element-et amin áll az aktuális játékos
+    */
     public void Slime(){
         s=(Saboteur)Game.getInstance().getCurrentCharacter();
         s.putSlime();
     }
 
+    /**
+     * Ezen a függvényen keresztül tudjuk beállítani a Pumpa be és kimenetét az aktuális játékossal
+     * @param src Bemenet száma (hanyadik szomszédja)
+     * @param dest Kimenet száma (hanyadik szomszédja)
+     */
     public void Adjust(int src, int dest){
         Game.getInstance().getCurrentCharacter().adjustPump(src, dest);
     }
 
+    /**
+     * Ezen a függvényen keresztül tudjuk befejezni az adott játékos körét, a következő játékos léphet.
+     */
     public void EndMove(){
         //TODO
     }
