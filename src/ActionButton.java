fceb494716250cbe703ce78a086f6f08bd53440b
Mark <mr.pc.mark@gmail.com>
2023-05-27 14:30:19 +0200

    ElementButton showActionButtonWindow(): ActionButton-ok megjelenitese dialogus ablakban. ActionButton osztaly

--- src/ActionButton.java
+++ src/ActionButton.java
@@ -6,17 +6,63 @@ import javax.swing.JButton;
 
 public class ActionButton extends JButton{
     private ArrayList<Integer> actions = new ArrayList<Integer>();
+    private Element element;
 
-    ActionButton(ArrayList<Integer> actions)
+    ActionButton(Element _element)
     {
-        this.actions=actions;
+        element=_element; 
+        /** Ezen keresztul tudjuk meghivni a Control fuggvenyeit.*/
+        Control instance = Control.getInstance();
+        
         addActionListener(
         new ActionListener()
         {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+                switch(e.getActionCommand()) {
+                case "Move": 
+                	/** Atadja a kivalasztott Elementet amire lepni szeretne.*/
+                	//instance.Move(_element);
+                	break;
+                	/** Valasztott Elementen lyukasztas.*/
+                case "Stab": 
+                	instance.Stab();
+                	break;
+                	/** Valasztott Elementen pumpa elhelyezes.*/
+                case "PlacePump": 
+                	instance.PlacePump();
+                	break;
+                	/** Valasztott Elementen cso elhelyezes.*/
+                case "PickupPipe": 
+                	instance.PlacePipe();
+                	break;
+                	/** Valasztott cso vegenek felvetele.*/
+                case "PickUpPipe": 
+                	//instance.PickUpPipe(_element);
+                	break;
+                	/** Valasztott Element javitasa.*/
+                case "Repair": 
+                	instance.Repair();
+                	break;
+                	/** Valasztott Element ragadossa tetele.*/
+                case "Stick": 
+                	instance.Stick();
+                	break;
+                	/** Valasztott Element csuszossa tetele.*/
+                case "Slime": 
+                	instance.Slime();
+                	break;
+                	/** Pumpa ki es bemenetenek allitasa.*/
+                case "Adjust": 
+                	//ez meg kerdeses
+                	break;
+                	/** Adott karakter korenek vege.*/
+                case "EndMove": 
+                	instance.EndMove();
+                	break;
+
+                }
+
             }
         });
     }
--- src/ElementButton.java
+++ src/ElementButton.java
@@ -7,6 +7,8 @@ import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
 
 public class ElementButton extends JButton{
     private ArrayList<ImageIcon> statusimages = new ArrayList<ImageIcon>();
@@ -30,7 +32,8 @@ public class ElementButton extends JButton{
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     // TODO Auto-generated method stub
-                    throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+                	showActionButtonWindow();
+                	//throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
                 }
         });
     }
@@ -44,5 +47,36 @@ public class ElementButton extends JButton{
             case "Pipe": return "";
         }
         return "";
+    }
+    
+    private void showActionButtonWindow() {
+        // Letrehozunk egy JDialog objektumot és beallitjuk a tulajdonságait
+        JDialog dialog = new JDialog();
+        dialog.setTitle("Valassz a muveletek kozul");
+        dialog.setModal(true); // Beallitjuk modalis ablaknak, amig bezarodik, a fiablak nem erheto el
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
+        // Letrehozunk egy JPanel objektumot az ActionButton-ok tarolasara
+        JPanel buttonPanel = new JPanel();
+
+        // ActionButton-ok letrehozasa es hozzaadasa a panelhoz
+        ActionButton actionButton1 = new ActionButton(element);
+        actionButton1.setActionCommand("Repair");
+        actionButton1.setText("Repair");
+        buttonPanel.add(actionButton1);
+
+        ActionButton actionButton2 = new ActionButton(element);
+        actionButton2.setActionCommand("Stab");
+        actionButton2.setText("Stab");
+        buttonPanel.add(actionButton2);
+        
+        // Panel hozzaadasa a dialogushoz
+        dialog.add(buttonPanel);
+
+        // A dialogus meretenek beallitasa a tartalomhoz
+        dialog.pack();
+
+        // A dialogus megjelenitese
+        dialog.setVisible(true);
     }
 }
