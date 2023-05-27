2582a284299dd4ff58497d9382c61aa473590673
Andai Roland <r.andai@outlook.com>
2023-05-27 15:39:18 +0200

    pipes added to elementbutton thing

--- src/ElementButton.java
+++ src/ElementButton.java
@@ -1,12 +1,16 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Image;
+import java.util.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class ElementButton extends JButton{
     private ArrayList<ImageIcon> statusimages = new ArrayList<ImageIcon>();
@@ -24,15 +28,9 @@ public class ElementButton extends JButton{
             System.out.println(ex);
         }
 
-        addActionListener(
-            new ActionListener()
-            {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    // TODO Auto-generated method stub
-                    throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
-                }
-        });
+        update();
+
+        addActionListener(e -> update()); // TODO: itt fog megjelenni az actionbutton
     }
 
     String getImageName(){
@@ -40,9 +38,56 @@ public class ElementButton extends JButton{
         switch(className){
             case "Cistern": return "bucket.png";
             case "WaterSource": return "fountain.png";
-            case "Pump": return "";
+            case "Pump": return "pump.png";
             case "Pipe": return "";
         }
         return "";
+    }
+
+    public void update(){
+        // Characterek az adott elemen
+        setLayout(null);
+        List<Character> cs = element.getStandingOn();
+        int offset = 0;
+        for(Character c : cs){
+            JLabel b = new JLabel();
+            String imgname;
+            if(c.getClass().getName().equals("Repairman")){
+                imgname = "man-mechanic.png";
+            }else{
+                imgname = "man-wearing-turban.png";
+            }
+            try {
+                Image img = ImageIO.read(getClass().getResource("img/" + imgname));
+                Image newimg = img.getScaledInstance( 20, 20,  java.awt.Image.SCALE_SMOOTH ) ;
+                b.setIcon(new ImageIcon(newimg));
+                if(element.getClass().getName().equals("Pipe")){
+                    b.setBounds(20, 20, 20, 20);
+                }else{
+                    b.setBounds(offset, 0, 20, 20);
+                }
+                offset += 20;
+                add(b);
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+
+    public ArrayList<ElementButton> getNeighboursElementButton(ArrayList<ElementButton> eb){
+        if(element.getClass().getName().equals("Pipe")){
+            List<Element> es = (List<Element>) element.getNeighbors();
+            ArrayList<ElementButton> toReturn = new ArrayList<ElementButton>();
+            for(Element eiter : es){
+                for(ElementButton ebiter : eb){
+                    if(ebiter.element.equals(eiter)){
+                        toReturn.add(ebiter);
+                    }
+                }
+            }
+            return toReturn;
+        }else{
+            return null;
+        }
     }
 }
--- src/Gui.java
+++ src/Gui.java
@@ -25,6 +25,7 @@ public class Gui {
     private JLabel lRepairmenPoints;
     private JButton endturn;
     private JTextField log;
+    JSpinner sPlayerCount;
     ArrayList<ActionButton> actionButtons;
     ArrayList<ElementButton> elementButtons;
 
@@ -57,7 +58,7 @@ public class Gui {
        sRoundSettings.setBounds(700, 100, 40, 30);
        menuPanel.add(sRoundSettings);
 
-       JSpinner sPlayerCount = new JSpinner(new SpinnerNumberModel(1, 1, 8, 1));
+        sPlayerCount = new JSpinner(new SpinnerNumberModel(1, 1, 8, 1));
        sPlayerCount.setBounds(700, 200, 40, 30);
        menuPanel.add(sPlayerCount);
 
@@ -71,9 +72,24 @@ public class Gui {
         
 
         //---------------MAIN GAME PANEL-----------------
-        gamePanel = new JPanel();
+        gamePanel = new JPanel(){
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                for(int i = 0; i < elementButtons.size(); i++){
+                    ElementButton eb = elementButtons.get(i);
+                    ArrayList<ElementButton> neigh = eb.getNeighboursElementButton(elementButtons);
+                    if(neigh != null){
+                        for(ElementButton n : neigh){
+                            drawLineBetweenButtons(eb, n, Color.BLACK, g);
+                        }
+                    }
+                }
+            }
+        };
         gamePanel.setLayout(null);
         gamePanel.setSize(1280, 720);
+        elementButtons = new ArrayList<ElementButton>();
         //Soronlevő játékos neve
         turn = new JLabel("NULL");
         turn.setBounds(40, 10, 0, 0);
@@ -88,30 +104,104 @@ public class Gui {
         log.setBounds(980, 100, 300, 500); // TODO: Pontos érték megadása ide
         gamePanel.add(log);
 
-        ElementButton cistern1 = new ElementButton(new Cistern(0));
-        cistern1.setBounds(200, 500, 50, 50);
+        // Első hat pályaelem
+        Cistern c1 = new Cistern(0);
+        ElementButton cistern1 = new ElementButton(c1);
+        cistern1.setBounds(200, 500, 90, 90);
+        elementButtons.add(cistern1);
+        Game.getInstance().addCistern(c1);
         gamePanel.add(cistern1);
 
-        ElementButton ws1 = new ElementButton(new WaterSource());
-        ws1.setBounds(200, 100, 50, 50);
+        WaterSource w1 = new WaterSource();
+        ElementButton ws1 = new ElementButton(w1);
+        ws1.setBounds(200, 100, 90, 90);
+        elementButtons.add(ws1);
+        Game.getInstance().addElement(w1);
         gamePanel.add(ws1);
 
-        ElementButton pump1 = new ElementButton(new Pump());
-        pump1.setBounds(200, 300, 50, 50);
+        Pump p1 = new Pump();
+        ElementButton pump1 = new ElementButton(p1);
+        pump1.setBounds(200, 300, 90, 90);
+        elementButtons.add(pump1);
+        Game.getInstance().addPump(p1);
         gamePanel.add(pump1);
 
-        ElementButton cistern2 = new ElementButton(new Cistern(0));
-        cistern2.setBounds(600, 500, 50, 50);
+        Cistern c2 = new Cistern(0);
+        ElementButton cistern2 = new ElementButton(c2);
+        cistern2.setBounds(600, 500, 90, 90);
+        elementButtons.add(cistern2);
+        Game.getInstance().addCistern(c2);
         gamePanel.add(cistern2);
 
-        ElementButton ws2 = new ElementButton(new WaterSource());
-        ws2.setBounds(600, 100, 50, 50);
+        WaterSource w2 = new WaterSource();
+        ElementButton ws2 = new ElementButton(w2);
+        ws2.setBounds(600, 100, 90, 90);
+        elementButtons.add(ws2);
+        Game.getInstance().addElement(w2);
         gamePanel.add(ws2);
 
-        ElementButton pump2 = new ElementButton(new Pump());
-        pump2.setBounds(600, 300, 50, 50);
+        Pump p2 = new Pump();
+        ElementButton pump2 = new ElementButton(p2);
+        pump2.setBounds(600, 300, 90, 90);
+        elementButtons.add(pump2);
+        Game.getInstance().addPump(p2);
         gamePanel.add(pump2);
 
+        //----------------PIPES--------------------------
+        Pipe pi1 = new Pipe(false, false, 0, 0, 0);
+        pi1.addNeighbor(w1);
+        w1.addNeighbor(pi1);
+        pi1.addNeighbor(p1);
+        p1.addNeighbor(pi1);
+        ElementButton pie1 = new ElementButton(pi1);
+        pie1.setBounds(215, 215, 60, 60);
+        elementButtons.add(pie1);
+        Game.getInstance().addPipe(pi1);
+        gamePanel.add(pie1);
+
+        Pipe pi2 = new Pipe(true, true, 0, 5, 5);
+        pi2.addNeighbor(c1);
+        c1.addNeighbor(pi2);
+        pi2.addNeighbor(p1);
+        p1.addNeighbor(pi2);
+        ElementButton pie2 = new ElementButton(pi2);
+        pie2.setBounds(215, 415, 60, 60);
+        elementButtons.add(pie2);
+        Game.getInstance().addPipe(pi2);
+        gamePanel.add(pie2);
+
+        Pipe pi3 = new Pipe(false, false, 0, 0, 0);
+        pi3.addNeighbor(c2);
+        c2.addNeighbor(pi3);
+        pi3.addNeighbor(p2);
+        p2.addNeighbor(pi3);
+        ElementButton pie3 = new ElementButton(pi3);
+        pie3.setBounds(615, 415, 60, 60);
+        elementButtons.add(pie3);
+        Game.getInstance().addPipe(pi3);
+        gamePanel.add(pie3);
+
+        Pipe pi4 = new Pipe(false, false, 0, 0, 0);
+        pi4.addNeighbor(w2);
+        w2.addNeighbor(pi4);
+        pi4.addNeighbor(p2);
+        p2.addNeighbor(pi4);
+        ElementButton pie4 = new ElementButton(pi4);
+        pie4.setBounds(615, 215, 60, 60);
+        elementButtons.add(pie4);
+        Game.getInstance().addPipe(pi4);
+        gamePanel.add(pie4);
+
+        Pipe pi5 = new Pipe(false, false, 0, 0, 0);
+        pi5.addNeighbor(p1);
+        p1.addNeighbor(pi5);
+        pi5.addNeighbor(p2);
+        p2.addNeighbor(pi5);
+        ElementButton pie5 = new ElementButton(pi5);
+        pie5.setBounds(415, 315, 60, 60);
+        elementButtons.add(pie5);
+        Game.getInstance().addPipe(pi5);
+        gamePanel.add(pie5);
 
         // ---------------END GAME FRAME-----------------
         endGamePanel = new JPanel();
@@ -142,6 +232,9 @@ public class Gui {
 
         //Redraw frame
         frame.repaint();
+        for(ElementButton e : elementButtons){
+            e.update();
+        }
     }
 
     /**
@@ -154,6 +247,20 @@ public class Gui {
         frame.getContentPane().removeAll();
         if(activePanel == menuPanel){
             activePanel = gamePanel;
+            int playerc = (Integer)sPlayerCount.getValue();
+            for(int i = 0; i < playerc; i++){
+                Character c;
+                if(i%2==0){
+                    Element e1 = Game.getInstance().getGameElements().get(0);
+                    c = new Repairman(e1, null, null);
+                    e1.addStandingOn(c);
+                }else{
+                    Element e2 = Game.getInstance().getGameElements().get(2);
+                    c = new Saboteur(e2);
+                    e2.addStandingOn(c);
+                }
+            }
+            updateFrame();
         }
         else{
             activePanel=endGamePanel;
@@ -183,14 +290,12 @@ public class Gui {
     }
 
     //Vonal rajzolása két gomb között, a megadott színnel
-    public void drawLineBetweenButtons(ElementButton b1, ElementButton b2, Color color) {
-        Graphics g = frame.getGraphics();
-
-        Graphics2D g2 = (Graphics2D) g;
+    public void drawLineBetweenButtons(ElementButton b1, ElementButton b2, Color color, Graphics g) {
+        Graphics2D g2 = (Graphics2D)g;
         g2.setStroke(new BasicStroke(2)); //2px Vastag
         g2.setColor(color);
-        Line2D lin = new Line2D.Float(b1.getAlignmentX(), b1.getAlignmentY(), b2.getAlignmentX(), b2.getAlignmentY());
-        g2.draw(lin);
+        int sth = (int)b1.getAlignmentX();
+        g2.drawLine(b1.getBounds().x + b1.getWidth()/2, b1.getBounds().y + b1.getHeight()/2, b2.getBounds().x + b2.getWidth()/2, b2.getBounds().y + b2.getHeight()/2);
     }
 
     
